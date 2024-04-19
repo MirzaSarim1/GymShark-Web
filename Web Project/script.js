9cba9162019c75d3eb708b6b77af13e41008d40e
MirzaSarim1 <mirzasarim.work@gmail.com>
2024-04-20 01:33:39 +0500

    Reviews Done

---
+++ Web Project/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#train1").click(function () {
+        $("#img-grid").attr("src", "images/training-image-01.jpg");
+        $("#img-p").text("First Training Class")
+    });
+});
+
+$(document).ready(function () {
+    $("#train2").click(function () {
+        $("#img-grid").attr("src", "images/training-image-02.jpg");
+        $("#img-p").text("Second Training Class")
+    });
+});
+
+$(document).ready(function () {
+    $("#train3").click(function () {
+        $("#img-grid").attr("src", "images/training-image-03.jpg");
+        $("#img-p").text("Third Training Class")
+    });
+});
+
+$(document).ready(function () {
+    $("#train4").click(function () {
+        $("#img-grid").attr("src", "images/training-image-04.jpg");
+        $("#img-p").text("Fourth Training Class")
+    });
+});
+
